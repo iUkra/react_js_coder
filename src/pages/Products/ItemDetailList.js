d58ed3fde126e4e3187701c644566521ffe1e6a9
Ukra <joantx.cs@gmail.com>
2023-09-26 19:10:57 -0300

    Mejoras estructurales

--- src/pages/Products/ItemDetailList.js
+++ src/pages/Products/ItemDetailList.js
@@ -1,81 +1,53 @@
-import FireBaseApi from "../../Components/ProductsApi"
-import { Link } from "react-router-dom";
-import {Image,  Divider, Header, Segment, Label} from "semantic-ui-react"
-import Category from '../Category/Category'
-import { Container, Row, Col  } from 'react-bootstrap';
-import ApiDolar from "../../Components/ApiDolar"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Image, Divider, Header, Segment, Label } from 'semantic-ui-react';
+import { Container, Row, Col } from 'react-bootstrap';
 import AOS from 'aos';
-// import UserApi from "../../Components/UserApi"
+import FireBaseApi from '../../Components/ProductsApi';
+import ApiDolar from '../../Components/ApiDolar';
+import Category from '../Category/Category';
+
 AOS.init();
-// className="justify-content-md-center"
+
 const Items = () => {
   const data = FireBaseApi();
   const dolarApi = ApiDolar();
-  let products
-  if (data!==undefined && dolarApi!==undefined) {
-    products = data.map(product => {
-      return (
-    <div  data-aos-delay='50' data-aos='fade-up' data-aos-offset='100' style={{width: '25rem'}} key={product.id}>
-      
-      {/* CAMBIAR ACA */}
-      {product.stock >=1 ? (
-      <div> 
-      <Col style={{paddingTop: '20px'}}>
-      <Segment>
-        <div>
-        <Header>
-        <Link to={`products/${product.id}`}><h6 style={{justifyContent: 'center', display: 'flex'}}>{product.title}</h6></Link>
-        </Header>
-        <Divider clearing />
-        <Link to={`products/${product.id}`}>
-        <Image style={{height: "150px"}} src={`${product.img[0]}`} rounded  centered />
-        </Link>
-        <Divider clearing />
-       
-        <div>
-        <div style={{display: "flex", justifyContent: "center"}}>
-        
-        <Label style={{alignSelf: "center"}} circular color={'green'} empty key={'green'} /> 
-        <p> En stock | ${product.price*dolarApi}</p>
-        </div>
-      </div>
-      </div>
-      </Segment>
-      
-      </Col> 
-      </div> 
-      ) : (
-      <div>
-      <Col style={{paddingTop: '20px'}}> 
-        <Segment>
-        <div> 
-        <Header>
-        <h6 style={{justifyContent: 'center', display: 'flex'}}>{product.title}</h6>
-        </Header>
-        <Divider clearing />
-        
-        <Image style={{height: "150px"}} src={`${product.img[0]}`} rounded  centered />
 
-        <Divider clearing />
-      <div>
-        <div style={{display: "flex", justifyContent: "center"}}>
-        <Label style={{alignSelf: "center"}} circular color={'red'} empty key={'red'} /> 
-        <p> Sin stock | ${product.price*dolarApi}</p>
-        </div>
-      </div>
-      </div> 
-      </Segment>
-      
-      </Col> 
-      </div> 
-      )}  
-   </div>
-      );
-    });
-  }
-  
+  // Función para renderizar los productos
+  const renderProduct = (product) => (
+    <div
+      data-aos-delay='50'
+      data-aos='fade-up'
+      data-aos-offset='100'
+      style={{ width: '25rem' }}
+      key={product.id}
+    >
+      <Col style={{ paddingTop: '20px' }}>
+        <Segment>
+          <div>
+            <Header>
+              <Link to={`products/${product.id}`}>
+                <h6 style={{ justifyContent: 'center', display: 'flex' }}>
+                  {product.title}
+                </h6>
+              </Link>
+            </Header>
+            <Divider clearing />
+            <Link to={`products/${product.id}`}>
+              <Image style={{ height: '150px' }} src={`${product.img[0]}`} rounded centered />
+            </Link>
+            <Divider clearing />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <Label style={{ alignSelf: 'center' }} circular color={product.stock >= 1 ? 'green' : 'red'} empty key={product.stock >= 1 ? 'green' : 'red'} />
+              <p>{product.stock >= 1 ? 'En stock' : 'Sin stock'} | ${product.price * dolarApi}</p>
+            </div>
+          </div>
+        </Segment>
+      </Col>
+    </div>
+  );
+
   return (
-  
     <Container style={{
       maxWidth: '100%',
       paddingRight: '0px',
@@ -85,27 +57,27 @@ const Items = () => {
       background: '#EAEAEA'
     }}>
       <Row>
-      <Col md={2}> <Category /> </Col>
-      {data===undefined ? (
-        <div className="d-flex col-md-12  justify-content-center">
-          <br></br>
-        <div style={{width: "100px",  height: "100px"}}  className="spinner-border" role="status">
-          <span  className="visually-hidden">Loading...</span>
-        </div>
-        </div>
-      ) : (
-        <Col md={10}>
-          <Container style={{ maxWidth: '100%'}}>
-          <Row style={{ maxWidth: '100%', justifyContent: 'center'}}>
-            {products}
-           </Row>
-          </Container>
-
+        <Col md={2}>
+          <Category />
         </Col>
-      )}
-    </Row>
+        {data === undefined ? (
+          <div className="d-flex col-md-12  justify-content-center">
+            <br></br>
+            <div style={{ width: '100px', height: '100px' }} className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        ) : (
+          <Col md={10}>
+            <Container style={{ maxWidth: '100%' }}>
+              <Row style={{ maxWidth: '100%', justifyContent: 'center' }}>
+                {data.map((product) => renderProduct(product))}
+              </Row>
+            </Container>
+          </Col>
+        )}
+      </Row>
     </Container>
- 
   );
 };
 
